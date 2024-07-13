c3184f543c48aaf8edaf29b069d20babf24d4e3e
Tung Dinh <tungxd301@gmail.com>
2024-07-12 22:07:06 -0700

    changes

--- src/layout/SideBar.tsx
+++ src/layout/SideBar.tsx
@@ -4,8 +4,8 @@ import { SideBarMenu } from "./SideBarMenu";
 export const SideBar = () => {
   return (
     <div className="flex-col hidden lg:flex fixed top-16 z-10 bg-white h-full">
-      <div className="h-[calc(100vh-4rem)] py-4 flex relative border-r border-r-gray-200/70">
-        <nav className="text-base lg:text-sm overflow-auto">
+      <div className="h-[calc(100vh-4rem)] flex relative border-r border-r-gray-200/70">
+        <nav className="text-base lg:text-sm overflow-auto py-4">
           <div className="flex flex-col text-xs lg:text-[10px] px-8 gap-21 uppercase font-medium pb-4">
             <p>Guidelines</p>
             <p className="text-gray-500">Updated: JULY 11, 2024</p>
